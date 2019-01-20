7b7a5efe7ea3ec2527a4009ae2c4d211b7e91512
Tijo Jose <tijojose27@yahoo.com>
2019-01-19 18:39:59 -0600

    addeds some buttons

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,35 @@ class MyApp extends StatelessWidget {
       title: 'Music UI App',
       theme: ThemeData(brightness: Brightness.dark),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Music App"),
-          backgroundColor: Colors.black87,
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.asset('images/background.jpg'),
-            Text("Albums"),
-            Text("Artists"),
-            Text("Songs")
-          ],
-        )
-      ),
+          appBar: AppBar(
+            title: Text("Music App"),
+            backgroundColor: Colors.black87,
+          ),
+          body: Column(
+            children: <Widget>[
+              Image.asset('images/background.jpg'),
+              ConstrainedBox(
+                  constraints: const BoxConstraints(minWidth: double.infinity),
+                  child: FlatButton(
+                    child: Text("Albums"),
+                    onPressed: () {},
+                  )),
+              ConstrainedBox(
+                constraints: const BoxConstraints(minHeight: double.infinity),
+                child: FlatButton(
+                  child: Text("Artists"),
+                  onPressed: () {},
+                )
+              ),
+              // ConstrainedBox(
+              //   constraints: const BoxConstraints(minHeight: double.infinity),
+              //   child: FlatButton(
+              //     child: Text("Songs"),
+              //     onPressed: (){},
+              //   ),
+              // ),
+            ],
+          )),
     );
   }
 }
